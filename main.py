5dc3e419803e41af33b98680dfc71eab1b8e7539
Scott Sanders <malgorath1@gmail.com>
2023-06-19 11:52:03 -0500

    Added facial defaults for tracking

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
+import cv2
 
-"""
-	This is the comment parts
-	Written by Scott Sanders <malgorath1@gmail.com> @ 2023
-	OscarAI is intend to demonstrate how to recognize and react to emotions using associations
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-"""
+video_capture = cv2.VideoCapture(0)
 
-import os
-import sys
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
 
-def main():
-	print("Hello World")
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
 
-if __name__ == '__main__':
-	sys.exit(main())
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture
+video_capture.release()
+cv2.destroyAllWindows()
